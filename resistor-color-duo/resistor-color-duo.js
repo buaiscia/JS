57dc46a92379c3d0a130f7bed4a51d164c09ebf8
Alex Buaiscia (HCI) <alex.buaiscia@gmail.com>
2020-01-27 20:23:44 +0100

    resistor-color-duo solved

--- resistor-color-duo/resistor-color-duo.js
+++ resistor-color-duo/resistor-color-duo.js
@@ -2,7 +2,30 @@
 // This is only a SKELETON file for the 'Resistor Color Duo' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
+//Black - Brown - Red - Orange - Yellow - Green - Blue - Violet - Grey - White
 
-export const decodedValue = () => {
-  throw new Error("Remove this statement and implement this function");
+export const decodedValue = (color) => {
+
+  let arrColor = [...color];
+
+  var colors = {
+    black: 0,
+    brown: 1,
+    red: 2,
+    orange: 3,
+    yellow: 4,
+    green: 5,
+    blue: 6,
+    violet: 7,
+    grey: 8,
+    white: 9
+  }
+
+  let [first, second] = [colors[arrColor[0]].toString(), colors[arrColor[1].toString()]];
+
+  return Number(first + second);
+  
+
 };
+
+
