d5b04cd3faa571b88841b59dacf5a160cbb2ba3e
Alex Buaiscia (HCI) <alex.buaiscia@gmail.com>
2020-01-27 20:03:21 +0100

    triangle exercise solved

--- triangle/triangle.js
+++ triangle/triangle.js
@@ -4,19 +4,43 @@
 //
 
 export class Triangle {
-  constructor() {
-    throw new Error("Remove this statement and implement this function");
+  constructor(side1, side2, side3) {
+    this.side1 = side1
+    this.side2 = side2
+    this.side3 = side3;
+    this.sides = [this.side1, this.side2, this.side3]
   }
 
   isEquilateral() {
-    throw new Error("Remove this statement and implement this function");
+      return this.isTriangle() && this.side1 === this.side2
+      && this.side2 === this.side3;
   }
 
   isIsosceles() {
-    throw new Error("Remove this statement and implement this function");
+      return this.isTriangle() &&
+      (this.side1 === this.side2 || this.side2 === this.side3
+        || this.side1 === this.side3);
   }
 
   isScalene() {
-    throw new Error("Remove this statement and implement this function");
+    if(this.isTriangle()) {
+      if(this.isEquilateral() || this.isIsosceles()) {
+        return false;
+      }
+      return true;
+    }
+    return false;
+  }
+
+  isTriangle() {
+    if(this.sides.includes(0)) {
+      return false;
+    }
+    if(this.side1 + this.side2 < this.side3 ||
+      this.side2 + this.side3 < this.side1 ||
+      this.side1 + this.side3 < this.side2) {
+        return false;
+      }
+      return true;
   }
 }
