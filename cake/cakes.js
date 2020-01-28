cdb7b1d2226df7c8c06b1c9210645fff9cba17a6
Alex Buaiscia (HCI) <alex.buaiscia@gmail.com>
2020-01-28 16:28:30 +0100

    cake_exercise

---
+++ cake/cakes.js
@@ -0,0 +1,20 @@
+export const cakes = (recipe, available) => {
+  let cakes = [];
+  let totCakes = 0;
+   for(ingredient in recipe) {
+
+      for(avail in available) {
+
+        if(!available.hasOwnProperty(ingredient)) {
+            return 0;
+          }
+         if(ingredient == avail) {
+              cakes.push(available[avail] / recipe[ingredient])
+           }
+      }
+      
+     }
+     console.log(cakes);
+     totCakes = Math.floor(Math.min(...cakes));
+     return totCakes;
+}
